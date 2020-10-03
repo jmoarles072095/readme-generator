2768d56ab5a19ee4259ce3ce0c88fdd9513e0f5a
Jose Morales <jmorales072095@gmail.com>
2020-10-02 20:40:14 -0700

    modified and arranged code

--- generateReadme.js
+++ generateReadme.js
@@ -1,30 +1,31 @@
 function generateMD(response) {
-    return `        # ${response.Title}
+    return `  
+# ${response.Title}
     
     
-    ## Description
-        ${response.Description}
+## Description
+    ${response.Description}
  
-        ## Table of Contents
-        * [Installation](#installation)
-        * [Usage](#usage)
-        * [License](#license)
-        * [Test](#test)
-        * [Contributors](#contributors)
-        * [Questions](#questions)
+## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Test](#test)
+    * [Contributors](#contributors)
+    * [Questions](#questions)
 
-    ## Installation
-        ${response.Installation}
-    ## Usage
-        ${response.Usage}
+## Installation
+    ${response.Installation}
+ ## Usage
+    ${response.Usage}
     
-    ## License
-        Lincenses used: ${response.License}
-    ## Test
-        ${response.Test}
-    ## Contributors
-        ${response.Contribuitions}
-    ## Questions
+## License
+    Lincenses used: ${response.License}
+## Test
+    ${response.Test}
+## Contributors
+    ${response.Contribuitions}
+## Questions
     GitHub: https://github.com/${response.Github}\n
     Contact Me: ${response.Email}
     `
